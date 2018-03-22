44f758c4e88dc6158e0edfcff805ab898307b88b
jinjeok <gudalsdl0421@gmail.com>
2018-03-22 10:39:45 +0900

    line break removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,6 @@ module.exports = {
             "error",
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
         "quotes": [
             "error",
             "single"
