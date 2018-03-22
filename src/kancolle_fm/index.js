a2fe49f4bdc440b83a0d902312b9991ef6742246
jinjeok <gudalsdl0421@gmail.com>
2018-03-22 13:56:55 +0900

    temp kancolle function added

--- src/kancolle_fm/index.js
+++ src/kancolle_fm/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+
 //This is index of Kancolle Fleet Management system
+
+/*
+"{user_id}": [
+    "{fleet_name}": [
+        "{kanmusu_name1}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },
+        "{kanmusu_name2}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },    // 1명 완료될 때마다 더 추가할건지 물어봄.
+        "{kanmusu_name3}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },
+        "{kanmusu_name4}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },
+        "{kanmusu_name5}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },
+        "{kanmusu_name6}": { "equipment": ["{equipment1}", "{equipment2}", "{equipment3}"], "type": "destroyer" },
+    ]
+]
+
+*/
