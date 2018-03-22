ea2e9cadba8c230d2d9a2baf23038b110fa8b556
jinjeok <gudalsdl0421@gmail.com>
2018-03-22 17:33:35 +0900

    Function test added

--- src/laboratory/index.js
+++ src/laboratory/index.js
@@ -2,13 +2,16 @@ const config = require('../../config.json');
 const prefix = config.prefix;
 module.exports = (client) => {
     client.on('message', message => {
-        if (message.content.startsWith(prefix + 'lab ')) {
+        if (message.content.startsWith(prefix + '말 ')) {
             message.delete()
                 .then(() => {
-                    message.channel.send(message.content.slice(5));
+                    message.channel.send(message.content.slice(3));
                 }, (reject) => {
                     message.channel.send('Error occured: `' + reject.message + '`.');
                 });
+        }
+        if (message.content.startsWith(prefix + '자르기 ')) {
+            message.channel.send(message.content.slice(message.content.search(/\s/)));
         }
     });
 };
