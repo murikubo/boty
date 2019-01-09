a07b0a0c39b23f4f984519b617991b19ad24f9d9
jinjeok <gudalsdl0421@gmail.com>
2019-01-09 17:43:00 +0900

    내일 하면 자동으로 내일 신간 나옴

--- src/crawling/index.js
+++ src/crawling/index.js
@@ -19,12 +19,13 @@ module.exports = (client) => {
         let parsed = util.slice(message.content);
         if(parsed.command == '북새통') {
             if(parsed.param) {
-                message.channel.send(util.embedFormat('북새통 명령어 사용법',[], '북새통의 신간을 확인할 수 있는 명령어입니다. \n내용에 `YYYY-MM-DD` 를 추가하여 해당 날짜의 신간을 확인할 수 있습니다. \n내용이 없거나 날짜 형태가 아니라면 오늘의 신간을 확인할 수 있습니다.'));
+                message.channel.send(util.embedFormat('북새통 명령어 사용법',[], '북새통의 신간을 확인할 수 있는 명령어입니다. \n내용에 `YYYY-MM-DD`를 입력하여 해당 날짜의 신간이 나옵니다.. \n내용에 `내일`을 입력하면 내일의 신간이 나옵니다. \n내용이 없거나 날짜 형태가 아니라면 오늘의 신간이 나옵니다.'));
                 return;
             }
             let now = new Date();
             let day;
             if(!parsed.content) day = date.format(now, 'YYYY-MM-DD');
+            else if (parsed.content == '내일') day = date.format(date.addDays(now,1), 'YYYY-MM-DD');
             else if (parsed.content && date.parse(parsed.content, 'YYYY-MM-DD')) day = parsed.content;
             else day = date.format(now, 'YYYY-MM-DD');
             axios({
