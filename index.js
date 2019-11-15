a317b2c85f48407732ce54f3efc3eb4df9b0a8d8
murikubo <skyaria2017@gmail.com>
2019-11-15 15:45:47 +0900

    Ver 7.5.0 - FFMPEG 업데이트 - 당분간 후미카씨의 음악 기능은 음악파일 재생 기능으로만 사용 부탁드립니다.

--- index.js
+++ index.js
@@ -125,10 +125,6 @@ client.on('message', message => {
         message.channel.send({
             embed: {
                 color: 3447003,
-                author: {
-                    name: client.user.username,
-                    icon_url: client.user.avatarURL
-                },
                 title: '공지',
                 description: "https://murikubo.github.io/shimushu/notice.html",
                 fields: [{
