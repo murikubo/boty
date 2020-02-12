2d5c6a67794abe9bd03441b39fc1004ecba9b665
jinjeok <gudalsdl0421@gmail.com>
2020-02-12 10:11:26 +0900

    메인페이지 만들다 때려침 신간 안내 함수화

--- src/crawling/index.js
+++ src/crawling/index.js
@@ -5,117 +5,202 @@ const Crawler = require('crawler');
 
 const namuCommand = ['나무위키', '나뮈키', '나무'];
 
+let newRelease = async (date) => {
+
+    return axios({
+        url: 'http://booksaetong.co.kr/shop/list.php?ca_id=90&gdate=' + date,
+        method: 'GET',
+        responseType: 'arraybuffer' 
+    }).then((res) => {
+        let bookarr = res.data.split(/<a href='\.\.\/shop\/item.php\?it_id=/g);
+        //let bookarr = res.data.split(/<a href='http:\/\/booksaetong.co.kr\/'/g);
+        bookarr.splice(0,1);
+        for(let i = 0; i <= bookarr.length-1; i++) {
+            bookarr[i] = bookarr[i].split('\n')[0];
+        }
+        return bookarr;
+        
+    }).then((bookarr) => {
+        let bookobj = [];
+        for(let i = 0; i < bookarr.length; i++) {
+            bookobj[i] = {
+                name: `${bookarr[i].slice(bookarr[i].search(/'>/)+2, bookarr[i].search('</a>'))}`,
+                id: `${bookarr[i].slice(0,bookarr[i].search(/'>/))}`,
+                saleprice: `${bookarr[i].slice(bookarr[i].search('<span class=amount>')+19, bookarr[i].search('</span>'))}`
+            };
+        }
+        return bookobj;
+    }).then((bookobj) => {
+        let returnobj = {};
+        returnobj.arr = [];
+        for(let i = 0; i < bookobj.length; i++) {
+            returnobj.arr[i] = {
+                name: bookobj[i].name,
+                value: `판매가: ${bookobj[i].saleprice}
+                [제품상세페이지](http://booksaetong.co.kr/shop/item.php?it_id=${bookobj[i].id})`,
+                inline: true
+            };
+        }
+        returnobj.arr = util.arrayCut(returnobj.arr);
+        returnobj.message = {};
+        returnobj.message.embed = {
+            author: {
+                name: 1 + '/' + returnobj.arr.length + ' 페이지',
+            },
+            title: date + ' 의 신간',
+            url: 'http://booksaetong.co.kr/shop/list.php?ca_id=90&gdate=' + date,
+            color: '3447003',
+            fields: returnobj.arr[0],
+            timestamp: new Date()
+        };
+        if(returnobj.arr.length == 0) {
+            returnobj.message.embed.fields = [{
+                name: '신간 페이지가 존재하지 않습니다.',
+                value: '아직 정보가 갱신되지 않았거나 해당 날짜의 신간이 존재하지 않습니다.'
+            }];
+        }
+        return returnobj;
+        
+    }).catch((err) => {
+        console.error(err);
+    });
+};
+
+/*
+return axios({
+    url: 'http://booksaetong.co.kr',
+    method: 'GET',
+    responseType: 'arraybuffer' 
+}).then(async (res) => {
+    let weekarr = res.data.split(/FuncWeekGo/g);
+    weekarr.splice(0,2);
+    let fields = [];
+    for(let i = 0; i <= weekarr.length-1; i++) {
+        weekarr[i] = weekarr[i].split('\n')[0].slice(2,12);
+    }
+    for(let i = 0; i <= weekarr.length-1; i++) {
+        fields.push({name: weekarr[i], value: `[링크](http://booksaetong.co.kr/shop/list.php?ca_id=90&gdate=${weekarr[i]})`, inline: true});
+    }
+    let content = {
+        embed: {
+            title: '북새통 메인 페이지',
+            url: 'http://booksaetong.co.kr/',
+            color: '3447003',
+            fields: fields,
+            timestamp: new Date()
+        }
+    };
+    console.log('1')
+    return [content, weekarr];
+    
+}).then(async (content) => {
+    
+    await message.channel.send(content[0])
+        .then(async (sentMessage) =>{
+
+            await sentMessage.react('◀')
+            await sentMessage.react('▶')
+            await sentMessage.react('❌')
+                .then(async () => {
+                    const collector = sentMessage.createReactionCollector((reaction, user) => {
+                        user.id === message.author.id &&
+                        reaction.emoji.name === '◀' ||
+                        reaction.emoji.name === '▶' ||
+                        reaction.emoji.name === '❌',
+                        {time: 30000};
+                    }).once('collect', reaction => {
+                        const chosen = reaction.emoji.name;
+                        if(chosen === '◀'){
+                            console.log('a')
+                            // Prev page
+                        }else if(chosen === '▶'){
+                            console.log('b')
+                            // Next page
+                        }else{
+                            console.log('c')
+                            // Stop navigating pages
+                        }
+                    });
+                    collector.on('end', () => {
+                        sentMessage.clearReactions();
+                        collector.stop();
+                    });
+                });
+        });
+});
+*/
+
 module.exports = (client) => {
     
-    client.on('message', message => {
+    client.on('message', async message => {
         let parsed = util.slice(message.content);
         if(parsed.command == '북새통') {
-            if(parsed.param) {
-                message.channel.send(util.embedFormat('북새통 명령어 사용법',[], '`북새통`명령어만 입력하면 오늘의 신간이 나옵니다. \n내용에 `어제`를 입력하면 어제의 신간이 나옵니다. \n내용에 `내일`을 입력하면 내일의 신간이 나옵니다. \n내용에 `YYYY-MM-DD`를 입력하면 해당 날짜의 신간이 나옵니다.'));
-                return;
-            }
-            let now = new Date();
-            let day;
-            if(!parsed.content) day = date.format(now, 'YYYY-MM-DD');
-            else if (parsed.content == '어제') day = date.format(date.addDays(now,-1), 'YYYY-MM-DD');
-            else if (parsed.content == '내일') day = date.format(date.addDays(now,1), 'YYYY-MM-DD');
-            else if (parsed.content && date.parse(parsed.content, 'YYYY-MM-DD')) day = parsed.content;
-            else if (date.parse(parsed.content, 'YYMMDD')) day = date.format(date.parse(parsed.content, 'YYMMDD'), 'YYYY-MM-DD');
-            else day = date.format(now, 'YYYY-MM-DD');
-            axios({
-                url: 'http://booksaetong.co.kr/shop/list.php?ca_id=90&gdate=' + day,
-                method: 'GET',
-                responseType: 'arraybuffer' 
-            }).then((res) => {
-                let bookarr = res.data.split(/<a href='\.\.\/shop\/item.php\?it_id=/g);
-                //let bookarr = res.data.split(/<a href='http:\/\/booksaetong.co.kr\/'/g);
-                bookarr.splice(0,1);
-                for(let i = 0; i <= bookarr.length-1; i++) {
-                    bookarr[i] = bookarr[i].split('\n')[0];
-                }
-                return bookarr;
-                
-            }).then((bookarr) => {
-                let bookobj = [];
-                for(let i = 0; i < bookarr.length; i++) {
-                    bookobj[i] = {
-                        name: `${bookarr[i].slice(bookarr[i].search(/'>/)+2, bookarr[i].search('</a>'))}`,
-                        id: `${bookarr[i].slice(0,bookarr[i].search(/'>/))}`,
-                        saleprice: `${bookarr[i].slice(bookarr[i].search('<span class=amount>')+19, bookarr[i].search('</span>'))}`
-                    };
-                }
-                return bookobj;
-            }).then((bookobj) => {
-                let content = [];
-                for(let i = 0; i < bookobj.length; i++) {
-                    content[i] = {
-                        name: bookobj[i].name,
-                        value: `판매가: ${bookobj[i].saleprice}
-                        [제품상세페이지](http://booksaetong.co.kr/shop/item.php?it_id=${bookobj[i].id})`,
-                        inline: true
-                    };
-                }
-                let index = 0;
-                content = util.arrayCut(content);
-                let embedObj = {
-                    embed: {
-                        author: {
-                            name: index+1 + '/' + content.length + ' 페이지',
-                        },
-                        title: day + ' 의 신간',
-                        url: 'http://booksaetong.co.kr/shop/list.php?ca_id=90&gdate=' + day,
-                        color: '3447003',
-                        fields: content[index],
-                        timestamp: new Date(),
-                        footer: {
-                            icon_url: client.user.avatarURL,
-                            text: '명령어 입력 시간'
-                        }
+            Promise.resolve()
+                .then(async () => {
+                    // command parsing phase
+                    if(parsed.param == 'h') {
+                        message.channel.send(util.embedFormat('북새통 명령어 사용법',[], '`북새통`명령어만 입력하면 오늘의 신간이 나옵니다. \n내용에 `어제`를 입력하면 어제의 신간이 나옵니다. \n내용에 `내일`을 입력하면 내일의 신간이 나옵니다. \n내용에 `YYYY-MM-DD`를 입력하면 해당 날짜의 신간이 나옵니다.'));
+                        return;
                     }
-                };
-                if(content.length == 0) {
-                    embedObj.embed.fields = [{
-                        name: '신간 페이지가 존재하지 않습니다.',
-                        value: '아직 정보가 갱신되지 않았거나 해당 날짜의 신간이 존재하지 않습니다.'
-                    }];
-                }
-                message.channel.send(embedObj)
-                    .then(async (sentMessage) => {
-                        if(content.length <= 1 ) {
-                            return;
-                        }
-                        await sentMessage.react('\u2B05')
-                            .then(() => {
-                                const collector = sentMessage.createReactionCollector((reaction, user) => reaction.emoji.name === '\u2B05' && user.id === message.author.id);
-                                collector.on('collect', (reaction) => {
-                                    if(index!=0)index--;
-                                    embedObj.embed.author.name = index+1 + '/' + content.length + ' 페이지';
-                                    embedObj.embed.fields = content[index];
-                                    sentMessage.edit(embedObj);
-                                    reaction.remove(message.author.id);
+                    let now = new Date();
+                    let parseDate;
+                    if(!parsed.content) parseDate = date.format(now, 'YYYY-MM-DD');
+                    else if (parsed.content == '어제') parseDate = date.format(date.addDays(now,-1), 'YYYY-MM-DD'); 
+                    else if (parsed.content == '내일') parseDate = date.format(date.addDays(now,1), 'YYYY-MM-DD'); 
+                    else if (parsed.content && date.parse(parsed.content, 'YYYY-MM-DD')) parseDate = parsed.content;
+                    else if (date.parse(parsed.content, 'YYMMDD')) parseDate = date.format(date.parse(parsed.content, 'YYMMDD'), 'YYYY-MM-DD'); 
+                    else  parseDate =  (date.format(now, 'YYYY-MM-DD')); 
+
+                    return await newRelease(parseDate);
+                }).then((releaseReturn) => {
+                    if(!releaseReturn) return;
+                    //more formating phase
+                    releaseReturn.message.embed.footer = {
+                        icon_url: client.user.avatarURL,
+                        text: '명령어 입력 시간'
+                    };
+                    
+                    return releaseReturn;
+                }).then((releaseReturn) => {
+                    if(!releaseReturn) return;
+                    //message send phase
+                    let index = 0;
+                    message.channel.send(releaseReturn.message)
+                        .then(async (sentMessage) => {
+                            if(releaseReturn.arr.length <= 1 ) {
+                                return;
+                            }
+                            await sentMessage.react('\u2B05')
+                                .then(() => {
+                                    const collector = sentMessage.createReactionCollector((reaction, user) => reaction.emoji.name === '\u2B05' && user.id === message.author.id);
+                                    collector.on('collect', (reaction) => {
+                                        if(index!=0)index--;
+                                        releaseReturn.message.embed.author.name = index+1 + '/' + releaseReturn.arr.length + ' 페이지';
+                                        releaseReturn.message.embed.fields = releaseReturn.arr[index];
+                                        sentMessage.edit(releaseReturn.message);
+                                        reaction.remove(message.author.id);
+                                    });
                                 });
-                            });
-                        await sentMessage.react('\u27A1')
-                            .then(() => {
-                                const collector = sentMessage.createReactionCollector((reaction, user) => reaction.emoji.name === '\u27A1' && user.id === message.author.id, { time: 30000 });
-                                collector.on('collect', (reaction) => {
-                                    if(content.length-1>index)index++;
-                                    embedObj.embed.author.name = index+1 + '/' + content.length + ' 페이지';
-                                    embedObj.embed.fields = content[index];
-                                    
-                                    sentMessage.edit(embedObj);
-                                    reaction.remove(message.author.id);
+                            await sentMessage.react('\u27A1')
+                                .then(() => {
+                                    const collector = sentMessage.createReactionCollector((reaction, user) => reaction.emoji.name === '\u27A1' && user.id === message.author.id, { time: 30000 });
+                                    collector.on('collect', (reaction) => {
+                                        if(releaseReturn.arr.length-1>index)index++;
+                                        releaseReturn.message.embed.author.name = index+1 + '/' + releaseReturn.arr.length + ' 페이지';
+                                        releaseReturn.message.embed.fields = releaseReturn.arr[index];
+                                        
+                                        sentMessage.edit(releaseReturn.message);
+                                        reaction.remove(message.author.id);
+                                    });
+                                    collector.on('end', () => sentMessage.clearReactions());
                                 });
-                                collector.on('end', () => sentMessage.clearReactions());
-                            });
-                    });
-            }).catch((err) => {
-                console.error(err);
-                message.channel.send('에러 또는 이 날에 신간이 없습니다. 혹시 모르니까 에러 메세지: ' + err);
-            });
+                        });
+
+                }).catch((err) => console.error(err));
+
         }
         if(namuCommand.includes(parsed.command)) {
-            let helpcommand = ['help', '도움말', '가이드', 'h']
+            let helpcommand = ['help', '도움말', '가이드', 'h'];
             if(helpcommand.includes(parsed.param)) {
                 message.channel.send(util.embedFormat('나무위키 명령어 사용법',[], '`나무위키` 혹은 `나뮈키` 혹은 `나무`와 `내용`을 입력하여 나무위키에서 찾습니다. \n만약 `내용`의 문서가 존재한다면 다이렉트 링크를 출력합니다. \n`내용`과 같은 문서가 없다면 검색 결과를 출력합니다. \n 다이렉트를 무시하고 검색만 하려면 `-검색`을 내용 전에 추가해야합니다.' ));
                 return;
